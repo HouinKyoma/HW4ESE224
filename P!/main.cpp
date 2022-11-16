d1291dd94247eb988d605d45bf46fa8ca2eb0daa
Hunter Zhong <hunterzhong929@gmail.com>
2022-11-15 19:41:28 -0500

    p1

--- P!/main.cpp
+++ P!/main.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
-
+#include<cstring>
+using namespace std;
+int SIZE = 20;
+void replaceB(char* s){
+    for(int i = 0;i<SIZE;i++){
+        if(s[i]=='b'){
+            s[i] = 'a';
+            
+        }
+        else if(s[i]=='g'){
+            s[i] = 'b';
+        }
+    }
+}
 int main(){
-    
+    char* string = new char[SIZE];
+    cout<<"input an array: "<<endl;
+    cin>>string;
+    replaceB(string);
+    cout<<string;
+    delete string;
 }
