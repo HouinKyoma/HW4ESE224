753fd4d648a368fb1bf2e451dab04d1fd236cc0e
Hunter Zhong <hunterzhong929@gmail.com>
2022-11-15 20:37:45 -0500

    restruct

---
+++ P3/queue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "node.h"
+#include "linked_list.h"
+class Queue:public LinkedList {
+  public:
+    void insert(std::string s) {
+
+    }
+    
+    void remove() {
+
+    }
+
+    void display() {
+
+    }
+};
